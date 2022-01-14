af88d27b150394baa1f168605303333eb5d4809d
lfelipe <rafael.eng.mecanic@gmail.com>
2022-01-14 18:33:12 -0300

    first commit

---
+++ src/main/java/com/firstprojectbootcamp/firstprojectbootcamp/FirstprojectbootcampApplication.java
@@ -0,0 +1,13 @@
+package com.firstprojectbootcamp.firstprojectbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectbootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectbootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstprojectbootcamp/firstprojectbootcamp/FirstprojectbootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstprojectbootcamp.firstprojectbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectbootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
