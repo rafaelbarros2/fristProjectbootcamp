32d320a6b13d0db5bc8d73d342d8cb5c599daa93
lfelipe <rafael.eng.mecanic@gmail.com>
2022-01-14 19:05:23 -0300

    resource

---
+++ src/main/java/com/firstprojectbootcamp/firstprojectbootcamp/resources/ClientResource.java
@@ -0,0 +1,53 @@
+package com.firstprojectbootcamp.firstprojectbootcamp.resources;
+
+import com.firstprojectbootcamp.firstprojectbootcamp.dtos.ClientDTO;
+import com.firstprojectbootcamp.firstprojectbootcamp.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        // parametros page, size, sort
+        Page<ClientDTO> list = service.findAllPaged(pageable);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) throws Exception {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+        ClientDTO newDTO = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(newDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+        ClientDTO newDTO = service.update(id, dto);
+        return ResponseEntity.ok().body(newDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
