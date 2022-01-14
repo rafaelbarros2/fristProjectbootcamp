240b486a8047ab06751a9a734a975cdc62860a51
lfelipe <rafael.eng.mecanic@gmail.com>
2022-01-14 18:55:28 -0300

    service, exceptions and repository

---
+++ src/main/java/com/firstprojectbootcamp/firstprojectbootcamp/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.firstprojectbootcamp.firstprojectbootcamp.repositories;
+
+import com.firstprojectbootcamp.firstprojectbootcamp.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
---
+++ src/main/java/com/firstprojectbootcamp/firstprojectbootcamp/services/ClientService.java
@@ -0,0 +1,80 @@
+package com.firstprojectbootcamp.firstprojectbootcamp.services;
+
+import com.firstprojectbootcamp.firstprojectbootcamp.dtos.ClientDTO;
+import com.firstprojectbootcamp.firstprojectbootcamp.entities.Client;
+import com.firstprojectbootcamp.firstprojectbootcamp.repositories.ClientRepository;
+import com.firstprojectbootcamp.firstprojectbootcamp.services.exceptions.DatabaseException;
+import com.firstprojectbootcamp.firstprojectbootcamp.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAllPaged(Pageable pageable) {
+        Page<Client> list = repository.findAll(pageable);
+        return list.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) throws Exception {
+        Optional<Client> obj = repository.findById(id);
+        Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+            Client entity = repository.getOne(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ClientDTO(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
+
+        entity.setChildren(dto.getChildren());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setCpf(dto.getCpf());
+        entity.setName(dto.getName());
+        entity.setIncome(dto.getIncome());
+    }
+}
